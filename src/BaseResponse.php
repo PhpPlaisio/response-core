f8ff343a7cc1e1049250cd2e049911718d9169d2
Paul Water <p.r.water@setbased.nl>
2020-12-06 08:09:00 +0100

    Drop support for PHP 7.3.

--- src/BaseResponse.php
+++ src/BaseResponse.php
@@ -62,79 +62,79 @@ class BaseResponse implements Response
    * @api
    * @since 1.0.0
    */
-  public static $httpStatuses = [100 => 'Continue',
-                                 101 => 'Switching Protocols',
-                                 102 => 'Processing',
-                                 118 => 'Connection timed out',
-                                 200 => 'OK',
-                                 201 => 'Created',
-                                 202 => 'Accepted',
-                                 203 => 'Non-Authoritative',
-                                 204 => 'No Content',
-                                 205 => 'Reset Content',
-                                 206 => 'Partial Content',
-                                 207 => 'Multi-Status',
-                                 208 => 'Already Reported',
-                                 210 => 'Content Different',
-                                 226 => 'IM Used',
-                                 300 => 'Multiple Choices',
-                                 301 => 'Moved Permanently',
-                                 302 => 'Found',
-                                 303 => 'See Other',
-                                 304 => 'Not Modified',
-                                 305 => 'Use Proxy',
-                                 306 => 'Reserved',
-                                 307 => 'Temporary Redirect',
-                                 308 => 'Permanent Redirect',
-                                 310 => 'Too many Redirect',
-                                 400 => 'Bad Request',
-                                 401 => 'Unauthorized',
-                                 402 => 'Payment Required',
-                                 403 => 'Forbidden',
-                                 404 => 'Not Found',
-                                 405 => 'Method Not Allowed',
-                                 406 => 'Not Acceptable',
-                                 407 => 'Proxy Authentication Required',
-                                 408 => 'Request Time-out',
-                                 409 => 'Conflict',
-                                 410 => 'Gone',
-                                 411 => 'Length Required',
-                                 412 => 'Precondition Failed',
-                                 413 => 'Request Entity Too Large',
-                                 414 => 'Request-URI Too Long',
-                                 415 => 'Unsupported Media Type',
-                                 416 => 'Requested range unsatisfiable',
-                                 417 => 'Expectation failed',
-                                 418 => 'I\'m a teapot',
-                                 422 => 'Unprocessable entity',
-                                 423 => 'Locked',
-                                 424 => 'Method failure',
-                                 425 => 'Unordered Collection',
-                                 426 => 'Upgrade Required',
-                                 428 => 'Precondition Required',
-                                 429 => 'Too Many Requests',
-                                 431 => 'Request Header Fields Too Large',
-                                 449 => 'Retry With',
-                                 450 => 'Blocked by Windows Parental Controls',
-                                 500 => 'Internal Server Error',
-                                 501 => 'Not Implemented',
-                                 502 => 'Bad Gateway or Proxy Error',
-                                 503 => 'Service Unavailable',
-                                 504 => 'Gateway Time-out',
-                                 505 => 'HTTP Version not supported',
-                                 507 => 'Insufficient storage',
-                                 508 => 'Loop Detected',
-                                 509 => 'Bandwidth Limit Exceeded',
-                                 510 => 'Not Extended',
-                                 511 => 'Network Authentication Required'];
+  public static array $httpStatuses = [100 => 'Continue',
+                                       101 => 'Switching Protocols',
+                                       102 => 'Processing',
+                                       118 => 'Connection timed out',
+                                       200 => 'OK',
+                                       201 => 'Created',
+                                       202 => 'Accepted',
+                                       203 => 'Non-Authoritative',
+                                       204 => 'No Content',
+                                       205 => 'Reset Content',
+                                       206 => 'Partial Content',
+                                       207 => 'Multi-Status',
+                                       208 => 'Already Reported',
+                                       210 => 'Content Different',
+                                       226 => 'IM Used',
+                                       300 => 'Multiple Choices',
+                                       301 => 'Moved Permanently',
+                                       302 => 'Found',
+                                       303 => 'See Other',
+                                       304 => 'Not Modified',
+                                       305 => 'Use Proxy',
+                                       306 => 'Reserved',
+                                       307 => 'Temporary Redirect',
+                                       308 => 'Permanent Redirect',
+                                       310 => 'Too many Redirect',
+                                       400 => 'Bad Request',
+                                       401 => 'Unauthorized',
+                                       402 => 'Payment Required',
+                                       403 => 'Forbidden',
+                                       404 => 'Not Found',
+                                       405 => 'Method Not Allowed',
+                                       406 => 'Not Acceptable',
+                                       407 => 'Proxy Authentication Required',
+                                       408 => 'Request Time-out',
+                                       409 => 'Conflict',
+                                       410 => 'Gone',
+                                       411 => 'Length Required',
+                                       412 => 'Precondition Failed',
+                                       413 => 'Request Entity Too Large',
+                                       414 => 'Request-URI Too Long',
+                                       415 => 'Unsupported Media Type',
+                                       416 => 'Requested range unsatisfiable',
+                                       417 => 'Expectation failed',
+                                       418 => 'I\'m a teapot',
+                                       422 => 'Unprocessable entity',
+                                       423 => 'Locked',
+                                       424 => 'Method failure',
+                                       425 => 'Unordered Collection',
+                                       426 => 'Upgrade Required',
+                                       428 => 'Precondition Required',
+                                       429 => 'Too Many Requests',
+                                       431 => 'Request Header Fields Too Large',
+                                       449 => 'Retry With',
+                                       450 => 'Blocked by Windows Parental Controls',
+                                       500 => 'Internal Server Error',
+                                       501 => 'Not Implemented',
+                                       502 => 'Bad Gateway or Proxy Error',
+                                       503 => 'Service Unavailable',
+                                       504 => 'Gateway Time-out',
+                                       505 => 'HTTP Version not supported',
+                                       507 => 'Insufficient storage',
+                                       508 => 'Loop Detected',
+                                       509 => 'Bandwidth Limit Exceeded',
+                                       510 => 'Not Extended',
+                                       511 => 'Network Authentication Required'];
 
   /**
    * If set redirect, client and server errors will not clear the output buffers. This variable is intended for unit
    * testing only (otherwise PhpUnit will label unit test as risky).
    *
-   * @var true|null
+   * @var bool
    */
-  public static $skipClearOutput;
+  public static bool $skipClearOutput = false;
 
   /**
    * The headers of the response.
@@ -144,7 +144,7 @@ class BaseResponse implements Response
    * @api
    * @since 1.0.0
    */
-  public $headers;
+  public ResponseHeader $headers;
 
   /**
    * The content of the response.
@@ -154,7 +154,7 @@ class BaseResponse implements Response
    * @api
    * @since 1.0.0
    */
-  protected $content;
+  protected string $content;
 
   /**
    * The status code of the response.
@@ -164,17 +164,16 @@ class BaseResponse implements Response
    * @api
    * @since 1.0.0
    */
-  protected $status;
+  protected int $status;
 
   /**
    * The HTTP protocol version (i.e. 1.0 or 1.1).
    *
    * @var string
    */
-  protected $version;
+  protected string $version = '1.1';
 
   //--------------------------------------------------------------------------------------------------------------------
-
   /**
    * Object constructor.
    *
@@ -183,10 +182,9 @@ class BaseResponse implements Response
    */
   public function __construct(string $content = '', int $status = 200)
   {
+    $this->content = $content;
     $this->headers = new ResponseHeader();
-    $this->setContent($content)
-         ->setStatus($status)
-         ->setVersion('1.1');
+    $this->status  = $status;
   }
 
   //--------------------------------------------------------------------------------------------------------------------
@@ -213,6 +211,18 @@ class BaseResponse implements Response
   public function getContent(): string
   {
     return $this->content;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the headers of the response. The keys of the array are the normalized header names and the values of the
+   * array are the corresponding values of the headers.
+   *
+   * @return array
+   */
+  public function getHeaders(): array
+  {
+    return $this->headers->getHeaders();
   }
 
   //--------------------------------------------------------------------------------------------------------------------
@@ -325,7 +335,7 @@ class BaseResponse implements Response
   /**
    * Sends the cookies to user agent.
    */
-  private function sendCookies()
+  private function sendCookies(): void
   {
     $this->headers->send();
     foreach (Nub::$nub->cookie as $cookie)
@@ -340,18 +350,6 @@ class BaseResponse implements Response
                                                   'sameSite' => $cookie->sameSite]);
       }
     }
-  }
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns the headers of the response. The keys of the array are the normalized header names and the values of the
-   * array are the corresponding values of the headers.
-   *
-   * @return array
-   */
-  public function getHeaders(): array
-  {
-    return $this->headers->getHeaders();
   }
 
   //--------------------------------------------------------------------------------------------------------------------
